6360e91ebe02e87af82865827e7501524982307f
6223602 <6223602@host.docker.internal>
2025-08-27 14:36:52 -0400

    first Commit

---
+++ src/main/java/com/mycompany/andy/FinalExam.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.andy;
+
+/**
+ *
+ * @author 6223602
+ */
+public class FinalExam extends GradedActivity {
+    
+}
---
+++ src/main/java/com/mycompany/andy/GradedActivity.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.andy;
+
+/**
+ *
+ * @author 6223602
+ */
+public class GradedActivity {
+
+    public static void main(String[] args) {
+        
+    }
+    
+    
+}
