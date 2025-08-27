458a5ed517aa838090f52f62b560c597fbb620cb
6223602 <6223602@host.docker.internal>
2025-08-27 15:22:01 -0400

    3rd Commit created all classes

---
+++ src/main/java/com/mycompany/andy/EssayClass.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.andy;
+
+/**
+ *
+ * @author 6223602
+ */
+public class EssayClass extends GradedActivity {
+    private double grammar;
+    private double spelling;
+    private double correctLength;
+    private double content;
+
+    public double getGrammar() {
+        return grammar;
+    }
+
+    public void setGrammar(double grammar) {
+        this.grammar = grammar;
+    }
+
+    public double getSpelling() {
+        return spelling;
+    }
+
+    public void setSpelling(double spelling) {
+        this.spelling = spelling;
+    }
+
+    public double getCorrectLength() {
+        return correctLength;
+    }
+
+    public void setCorrectLength(double correctLength) {
+        this.correctLength = correctLength;
+    }
+
+    public double getContent() {
+        return content;
+    }
+
+    public void setContent(double content) {
+        this.content = content;
+    }
+   
+}
--- src/main/java/com/mycompany/andy/FinalExam.java
+++ src/main/java/com/mycompany/andy/FinalExam.java
@@ -9,5 +9,17 @@ package com.mycompany.andy;
  * @author 6223602
  */
 public class FinalExam extends GradedActivity {
+    private int numQuestions;
+    private double pointsEach;
+    private int numMissed;
     
+    
+
+    public double getPointsEach() {
+        return pointsEach;
+    }
+
+    public int getNumMissed() {
+        return numMissed;
+    }
 }
